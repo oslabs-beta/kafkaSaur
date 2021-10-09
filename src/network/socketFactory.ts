878cf8ac79069e05ac2fd5722ebdea049d4c92e0
Sam Arnold <sama@Sams-MacBook-Pro.local>
2021-10-09 16:55:42 -0400

    refactored tcp-packet module

--- src/network/socketFactory.ts
+++ src/network/socketFactory.ts
@@ -3,21 +3,25 @@ const KEEP_ALIVE_DELAY = 60000 // in ms
 /**
  * @returns {import("../../types").ISocketFactory}
  */
-import net from 'https://raw.githubusercontent.com/cmorten/deno_std/b0ba0face0b5cbb67d36c05181e94e0a839ec7dd/node/net.ts'
-import { Client, Event, Packet } from "https://deno.land/x/tcp_socket@0.0.2/mods.ts";
+//import net from 'https://raw.githubusercontent.com/cmorten/deno_std/b0ba0face0b5cbb67d36c05181e94e0a839ec7dd/node/net.ts'
+
+import { Client } from './socketClass.ts'
+import { EventEmitter } from "https://deno.land/std@0.110.0/node/events.ts";
 
 export default () => {
 
   return async ({ host, port, onConnect}: any) => { //async await????
-    const socket = net.connect({ host, port }, onConnect)
-    // await new Client({hostname: host, port: port})
-    // // console.log("SAM SOCKET", socket);
-    // socket.on(Event.connect, onConnect)
-    // socket.connect();
+    
+    const socket = new Client({hostName: host, port: port})
+    
+    
+    socket.on('connect', onConnect)
+    socket.connect();
+    socket.on('connect', () => console.log('I connected'))
     // // socket.on(Event.connect, onConnect)
     // // console.log('socket inside of SF', socket)
 
-    socket.setKeepAlive(true, KEEP_ALIVE_DELAY)
+    //socket.setKeepAlive(true, KEEP_ALIVE_DELAY)
 
     return socket;
   };
